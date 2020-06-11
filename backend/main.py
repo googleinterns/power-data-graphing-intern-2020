cf7758f257b1405d2593cebada5eb4a72e571a56
Yifei Tang <tangyifei@google.com>
2020-06-11 15:25:43 -0400

    refactored backend code

--- backend/main.py
+++ backend/main.py
@@ -1,26 +1,57 @@
 # Simple backend server using flask framework
 
-from flask import Flask, jsonify, request
-from flask_cors import CORS, cross_origin
+from flask import request
+from flask import jsonify
+from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
-'''
-    Get sample data from local file with a fixed output lines
-    TODO: too naive, improvement required.
-'''
-@app.route('/data')
-def getData():
-    fn = 'Power_sample_data.csv'
-    numLines = 7200000  # assume number of records is accessible upon deployment
-    maxLines = request.args.get('number', default=6000, type=int)
-    frequency = numLines / maxLines
+
+def downsample_raw_data(file_name, num_records, max_records):
+    '''Desample power data
+
+        Read power data from given filename, desample and limit number of records to the given number.
+        The desample strategy is to select one data point per certain number to ensure that output 
+        data points are kept under threshold.
+
+        Args:
+            file_name: The filename of data csv
+            num_records: Total number of data points 
+            max_records: Max number of data points to output
+        
+        Returns:
+            A list of power data. For example:
+            [
+                ['1532523212', '53', 'SYSTEM'],
+                ['1532523242', '33', 'SYSTEM']
+            ]
+    '''
     data = list()
-    with open(fn, 'r') as fr:
+    frequency = num_records / max_records
+
+    with open(file_name, 'r') as fr:
         for i, line in enumerate(fr):
             if i % frequency == 0:
                 data.append(line.strip('\n').split(','))
+    return data
+    
+
+
+@app.route('/data')
+def getData():
+    '''HTTP endpoint to get data
+        
+        Retrives all power data from local file given a limit on number of records from request body.
+    '''
+
+    fn = 'Power_sample_data.csv'
+    num_records = 7200000  # assume number of records is accessible upon deployment
+    max_records = request.args.get('number', default=6000, type=int)
+    
+    data = downsample_raw_data(fn, num_records, max_records)
+    # TODO(tangyifei@): Implementation too naive, will improve with selected time interval, better desample strategy.
     return jsonify(data)
 
 
